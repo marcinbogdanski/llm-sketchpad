9adc77480f2330f9a2e4ab94dedbb5d97d52ac29
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2026-07-09 13:57:31 +0000

    Run mini param sweep, update some LRs slightly

--- posts/20260702-muon-optimizer/dev/optim_0_builtin.py
+++ posts/20260702-muon-optimizer/dev/optim_0_builtin.py
@@ -2,7 +2,7 @@ import torch
 
 OPTIMIZERS_OWN_COMMS = False  # Trainer script needs to wrap model in DDP
 
-def setup_optimizers(model, embedding_lr=0.1, unembedding_lr=0.01, matrix_lr=0.02, weight_decay=0.0):
+def setup_optimizers(model, embedding_lr=0.3, unembedding_lr=0.003, matrix_lr=0.02, weight_decay=0.0):
     """Prepare param groups and setup optimizers. Scale learning rates based on parameter counts"""
     assert isinstance(model, torch.nn.Module)
 
